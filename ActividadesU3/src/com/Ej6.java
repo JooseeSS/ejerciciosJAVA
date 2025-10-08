b79a24c0cc9c0f1b37a7fdaa1c3bff8aa24ed41e
unknown <serrano.santos.jose.antonio@iescamas.es>
2025-10-08 20:08:21 +0200

    Subida de ejercicios

--- ActividadesU3/src/com/Ej6.java
+++ ActividadesU3/src/com/Ej6.java
@@ -5,7 +5,18 @@ public class Ej6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 /*Escribir todos los múltiplos de 7 menores que 100.*/
-
+int n=7;
+int multi=0;
+System.out.println("Estos son los multiplos de 7 menores que 100");
+for (int i = 0; multi <=100; i++) {
+ multi = n*i;
+ if (multi<=100) {
+	 System.out.println(multi);
+}
+ 
+}
+		
+		
 	}
 
 }
---
+++ ActividadesU3/src/com/Ej8.java
@@ -0,0 +1,28 @@
+package com;
+import java.util.*;
+public class Ej8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new  Scanner(System.in);
+/*
+ * Pedir un número y calcular su factorial. 
+ * Por ejemplo, el factorial de 5 se denota 5! y es igual a 5 × 4 × 3 × 2 × 1 = 120.
+ */
+		int fact=1;
+	System.out.println("De que numero quiere calcular su factorial");	
+	int num = sc.nextInt();	
+	
+	for (int i =1; i<=num; i++) {
+		 fact *=i;
+		}
+		System.out.println("El factorial de " +num +" es " +fact);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
